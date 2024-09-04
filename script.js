671bcc52e8e38a60bde78a345ae4e86bed564a3f
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-04 19:11:50 +0200

    added markup and imgList[ ]

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-//Check link
-console.log('JS OK')
-
-// 1.recupero l'elemento dal Dom
-
-
-//2.raccolta dati
-    
-// 3.lavorazione dati
-    
-
-// 4.generazione output
-
-
+// vue-js
 const { createApp } = Vue
 
   createApp({
     data() {
       return {
-        message: 'Hello Vue!',
+        imgList: [
+          {
+              image: 'img/01.webp',
+              title: 'Marvel\'s Spiderman Miles Morale',
+              text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+          },
+          {
+              image: 'img/02.webp',
+              title: 'Ratchet & Clank: Rift Apart',
+              text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+          },
+          {
+              image: 'img/03.webp',
+              title: 'Fortnite',
+              text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+          },
+          {
+              image: 'img/04.webp',
+              title: 'Stray',
+              text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+          },
+          {
+              image: 'img/05.webp',
+              title: "Marvel's Avengers",
+              text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+          },
+      ],
       }
     }
   }).mount('#app')
